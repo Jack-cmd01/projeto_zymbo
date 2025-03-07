eac934ca6f4a3774a330e103a1046260f15867e1
João Carlos <jackworktoo@gmail.com>
2025-03-07 15:12:25 -0300

    Ascensão
    Atualização: (Front-end) 'crie sua conta' - 'acesse sua conta' -  'crie seu evento' - Cards e footer.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Função Carrossel
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -24,3 +26,19 @@ function showSlides(n) {
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " ativo";
 }    
+
+
+// Função de +Ver Tudo
+
+document.querySelector(".verTudo").addEventListener("click", function() {
+  let ocultos = document.querySelectorAll(".cartoes2.oculto");
+  let botao = this;
+
+  let visivel = Array.from(ocultos).some(card => card.style.display === "block");
+
+  ocultos.forEach(card => {
+      card.style.display = visivel ? "none" : "block";
+  });
+
+  botao.textContent = visivel ? "+ Ver tudo" : "- Ver menos";
+});
