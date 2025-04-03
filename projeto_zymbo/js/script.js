c504fe5e6fb8a3455979ee5721ae70d6574fc42b
João Carlos <jackworktoo@gmail.com>
2025-04-03 15:38:10 -0300

    Treinamento.1
    Estrutura de HTML e CSS alterada para maior fluidez e organização.

---
+++ projeto_zymbo/js/script.js
@@ -0,0 +1,45 @@
+// Função Carrossel
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function mudarSlide(n) {
+  showSlides(slideIndex += n);
+}
+
+function slide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slidesImg");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" ativo", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " ativo";
+}    
+
+
+// Função de +Ver Tudo
+
+document.querySelector(".verTudo").addEventListener("click", function() {
+  let ocultos = document.querySelectorAll(".cartoes2.oculto");
+  let botao = this;
+
+  let visivel = Array.from(ocultos).some(card => card.style.visibility === "visible");
+
+  ocultos.forEach(card => {
+    card.style.visibility = visivel ? "hidden" : "visible";
+    card.style.position = visivel ? "absolute" : "relative";
+  });
+
+  botao.textContent = visivel ? "+ Ver tudo" : "- Ver menos";
+});
